b929939bd74fdf3315f651d4a76aa513fb372007
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-12-18 19:34:17 +0100

    letsencrypt

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 
 **Self-hosted, music streaming platform**
 
-## Setup
-* Default login:
-    ```
-    username: forte
-    password: alternative
-    ```
+## Server Setup
+1. `git clone https://github.com/kaangiray26/forte.git`
+2. `cd forte/server`
+3. `npm install`
+4. `npx greenlock init --config-dir ./greenlock.d --maintainer-email '<your email here>'`
+5. `npx greenlock add --subject yourdomain.tld --altnames yourdomain.tld`
+6. Change the library path in `server/config.json`
+7. Install and start a postgresql database with a database named `forte` with a user named `forte` on `localhost:5432`
+8. `sudo node start`
---
+++ server/index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const session = require('express-session')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const cors = require('cors')
+const path = require('path')
+const crypto = require('crypto')
+const db = require('./js/db.js')
+
+const app = express()
+
+app.use(cors({ credentials: true, origin: true }))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+    secret: crypto.randomBytes(64).toString('hex'),
+    resave: false,
+    saveUninitialized: true,
+}))
+
+app.use(express.static('public/assets'))
+app.use(express.static('uploads'))
+
+function isAdmin(req, res, next) {
+    if (req.session.user && req.session.user == 'forte') next()
+    else next('route')
+}
+
+function isAuthenticated(req, res, next) {
+    db.is_authenticated(req.session.id).then((ok) => {
+        if (ok) next()
+        else res.status(401).json({ "error": "session expired." })
+    })
+}
+
+app.get("/", isAdmin, (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/login.html'))
+})
+
+app.post("/login", db.login)
+
+app.get("/log_off", (req, res, next) => {
+    req.session.user = null
+    res.sendStatus(200)
+})
+
+app.get("/get_users", isAdmin, db.get_users)
+
+app.post("/add_user", isAdmin, db.add_user)
+
+app.post("/remove_user", isAdmin, db.remove_user)
+
+// API
+
+app.get("/api/test", isAuthenticated, (req, res, next) => {
+    res.status(200).json({ "success": "session up to date." })
+})
+
+app.get("/api/auth", db.auth)
+app.get("/api/save_session", db.auth)
+
+app.post("/api/cover", isAuthenticated, upload.single('cover'), db.upload_cover)
+
+app.get("/api/search/:query", isAuthenticated, db.search)
+app.get("/api/stream/:id", isAuthenticated, db.stream)
+
+app.get("/api/profile/", isAuthenticated, db.get_profile)
+app.get("/api/user/:id", isAuthenticated, db.get_user)
+
+app.get("/api/track/:id", isAuthenticated, db.get_track)
+app.get("/api/track/:id/basic", isAuthenticated, db.get_track_basic)
+
+app.get("/api/artist/:id", isAuthenticated, db.get_artist)
+
+app.get("/api/album/:id", isAuthenticated, db.get_album)
+app.get("/api/album/:id/tracks", isAuthenticated, db.get_album_tracks)
+
+module.exports = app;
