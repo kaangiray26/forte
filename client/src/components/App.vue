c62db81f9f9aed49a3a95d77dbe7224f9506c24d
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2023-02-13 18:37:18 +0100

    phone height

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -79,6 +79,10 @@ async function keyPress(event) {
 }
 
 onMounted(() => {
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
     window.focus();
     window.addEventListener('keydown', keyPress);
 })
