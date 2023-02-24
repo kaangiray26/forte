10051b7d9448e87b191416fc5623189f835d9ba1
kaangiray26 <kaangiray26@protonmail.com>
2023-02-24 14:45:53 +0100

    trying out some things #55

--- server/server.js
+++ server/server.js
@@ -1,20 +1,57 @@
 'use strict';
 
 import app from './js/index.js';
-import greenlock from 'greenlock-express';
+import greenlock from 'greenlock'
+import greenlock_express from 'greenlock-express';
 
-greenlock
-    .init({
-        packageRoot: './',
+process.env.email = "kaangiray26@protonmail.com"
+process.env.domain = "home.buzl.uk"
 
-        // where to look for configuration
-        configDir: './greenlock.d',
+const g = greenlock.create({
+    packageRoot: './',
+    configDir: './greenlock.d',
+    maintainerEmail: process.env.email,
+    cluster: false
+})
 
-        maintainerEmail: 'maintain@example.com',
-
-        // whether or not to run at cloudscale
-        cluster: false
+async function init() {
+    // Greenlock configuration
+    g.manager.defaults({
+        agreeToTerms: true,
+        subscriberEmail: process.env.email
     })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
+
+    // Check site status
+    let sites = await g.sites.get({
+        servername: process.env.domain
+    })
+
+    // Add site if not exists
+    if (!sites) {
+        // Create site
+        await g.sites.add({
+            subject: process.env.domain,
+            altnames: [process.env.domain]
+        })
+    }
+
+    // Start server
+    greenlock_express
+        .init({
+            packageRoot: './',
+
+            // where to look for configuration
+            configDir: './greenlock.d',
+
+            maintainerEmail: process.env.email,
+
+            // whether or not to run at cloudscale
+            cluster: false
+        })
+        // Serves on 80 and 443
+        // Get's SSL certificates magically!
+        .serve(app);
+}
+
+// onMounted
+init();
