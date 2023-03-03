7c6c6007048129df7d3ac04f2ac2219aa3506460
kaangiray26 <kaangiray26@protonmail.com>
2023-03-04 00:22:23 +0100

    reverse proxy

--- server/server.js
+++ server/server.js
@@ -2,58 +2,6 @@
 
 import app from './js/index.js';
 import db from "./js/db.js";
-import greenlock from 'greenlock'
-import greenlock_express from 'greenlock-express';
-
-async function setup() {
-    const g = greenlock.create({
-        packageRoot: './',
-        configDir: './greenlock.d',
-        maintainerEmail: process.env.forte_email,
-        cluster: false,
-    })
-
-    // Greenlock configuration
-    await g.manager.defaults({
-        agreeToTerms: true,
-        subscriberEmail: process.env.forte_email
-    })
-
-    // Check site status
-    let sites = await g.sites.get({
-        servername: process.env.forte_server
-    })
-
-    // Add site if not exists
-    if (!sites) {
-        // Create site
-        await g.sites.add({
-            subject: process.env.forte_server,
-            altnames: [process.env.forte_server]
-        })
-    }
-    db.init(process.argv.slice(2));
-}
-
-async function start_greenlock_server() {
-    console.log("\x1b[32m%s\x1b[0m", `..: Server:    http://${process.env.forte_server}/ :..`);
-    // Start server
-    greenlock_express
-        .init({
-            packageRoot: './',
-
-            // where to look for configuration
-            configDir: './greenlock.d',
-
-            maintainerEmail: process.env.forte_email,
-
-            // whether or not to run at cloudscale
-            cluster: false
-        })
-        // Serves on 80 and 443
-        // Get's SSL certificates magically!
-        .serve(app);
-}
 
 async function start_public_server() {
     app.listen(process.env.port, '0.0.0.0', () => {
@@ -68,11 +16,6 @@ async function init() {
     switch (process.env.mode) {
         case 'public':
             start_public_server();
-            break;
-
-        case 'greenlock':
-            await setup();
-            start_greenlock_server();
             break;
 
         default:
